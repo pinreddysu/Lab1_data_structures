9aa36ff86e2a70c7d5cc4db190a3eb9c9f1a2706
Suchith Chandan Reddy Pinreddy <70670543+pinreddysu@users.noreply.github.com>
2022-03-05 17:45:14 -0600

    fun1 function

--- problem1.c
+++ problem1.c
@@ -23,3 +23,18 @@ int main (void) {
     return (0);
 }
 
+/*
+    NAME: fun1
+    PARAMETERS: Double data type Pointers *smp and *lgp
+    PURPOSE: The function swaps the numbers in lowest to greatest in the pointers
+    PRECONDITION: The parameter of this function is to input the address of the double data type variable
+    POSTCONDITION: The value will swap if the *smp (small number pointer) is greater than *lgp (large number pointer)
+*/
+void fun1 (double *smp, double *lgp) { //*smp small number pointer *lgp large number pointer
+    double temp;
+    if (*smp > *lgp) {
+    temp=*smp;
+    *smp=*lgp;
+    *lgp=temp;
+    }
+}
