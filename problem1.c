55b00992f760d36cc6b09cde9827c6620419ad10
Suchith Chandan Reddy Pinreddy <70670543+pinreddysu@users.noreply.github.com>
2022-03-05 17:44:44 -0600

    insertion of main function

--- problem1.c
+++ problem1.c
@@ -6,4 +6,20 @@
 
 //Declaration of function fun1
 void fun1(double *smp, double *lgp);
+/*
+ AUTHOR: SUCHITH CHANDAN REDDY PINREDDY
+ FILENAME: problem1.c
+ SPECIFICATION: THE PROGRAM SWAPS THE NUMBER IF THE FIRST NUMBER IS GREATER THAN THE SECOND NUMBER.
+ FOR: CS 2413 Data Structures Section 002
+*/
+int main (void) {
+    double num1, num2, num3;
+    printf("Enter 3 numbers separated by blanks> ");
+    scanf("%lf%lf%lf",&num1,&num2,&num3);
+    fun1(&num1,&num2);
+    fun1(&num1,&num3);
+    fun1(&num2,&num3);
+    printf("The numbers are: %.2f %.2f %.2f\n", num1, num2, num3);
+    return (0);
+}
 
