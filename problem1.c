b16e3542f3ac9a4a25a84a3c46a3869b5a9dcaad
Suchith Chandan Reddy Pinreddy <70670543+pinreddysu@users.noreply.github.com>
2022-03-05 17:54:22 -0600

    Update problem1.c

--- problem1.c
+++ problem1.c
@@ -39,4 +39,71 @@ void fun1 (double *smp, double *lgp) { //*smp small number pointer *lgp large nu
     }
 }
 
-
+/*
+ANSWERS:
+-----> 1B Answer:
+//Declaration of function fun1
+void fun1(double *smp, double *lgp);
+//Main Function
+int main (void) {
+    double num1, num2, num3;
+    printf("Enter 3 numbers separated by blanks> ");
+    scanf("%lf%lf%lf",&num1,&num2,&num3);
+    fun1(&num1,&num2);
+    fun1(&num1,&num3);
+    fun1(&num2,&num3);
+    printf("The numbers are: %.2f %.2f %.2f\n", num1, num2, num3);
+    return (0);
+}
+//Fun1 Function
+void fun1 (double *smp, double *lgp) {
+    double temp;
+    if (*smp > *lgp) {
+    temp=*smp;
+    *smp=*lgp;
+    *lgp=temp;
+    }
+}
+--------------------------------------------------------------------------------------------------------------------------------------
+----->Code Runs:
+Enter 3 numbers separated by blanks> 78
+56
+41
+The numbers are: 41.00 56.00 78.00
+The code is sorting the numbers inputed in lowest to greatest using pointers
+--------------------------------------------------------------------------------------------------------------------------------------
+-----> 1C Answer:
+//Declaration of function fun1
+void fun1(double smp, double lgp);
+//Main Function
+int main (void) {
+    double num1, num2, num3;
+    printf("Enter 3 numbers separated by blanks> ");
+    scanf("%lf%lf%lf",&num1,&num2,&num3);
+    fun1(num1,num2);
+    fun1(num1,num3);
+    fun1(num2,num3);
+    printf("The numbers are: %.2f %.2f %.2f\n", num1, num2, num3);
+    return (0);
+}
+//Fun1 Function
+void fun1 (double smp, double lgp) {
+    double temp;
+    if (smp > lgp) {
+    temp=smp;
+    smp=lgp;
+    lgp=temp;
+    }
+}
+-------------------------------------------------------------------------------------------------------------------------------------
+--------->Code Runs
+Enter 3 numbers separated by blanks> 98
+104
+1
+The numbers are: 98.00 104.00 1.00
+-------------------------------------------------------------------------------------------------------------------------------------
+---------->Explanation:
+The code does not sort the numbers due to the loss of pointers since program cannot use memory allocation which makes main function
+variables not to link with the fun1 variable causing numbers to not change.
+--------------------------------------------------------------------------------------------------------------------------------------
+*/
